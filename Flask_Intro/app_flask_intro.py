48a4e21e337dba2e0918bdf78436350a2b9d4f92
Jorge Clarke <JorgeClarke@github.com>
2021-10-06 09:42:22 +0200

    I did it again

--- Flask_Intro/app_flask_intro.py
+++ Flask_Intro/app_flask_intro.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)   # This is for referencing this file (Auto referencing ?)
 
 @app.route('/')     # index route, so when we browse to the URL we don't immediatelly just 404.
                     # Flask set-up routes with app route decorator. We are going to pass the URL string of our route
 def index():        # Define the function for the previous route
-    return "Hello, world!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)     # = True, so if we have any errors they will pop-up and we can see
