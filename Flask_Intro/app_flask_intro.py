646263910b37332754fe1e06d808c5e2d09a72a4
Jorge Clarke <JorgeClarke@github.com>
2021-10-07 12:43:18 +0200

    Init app

--- Flask_Intro/app_flask_intro.py
+++ Flask_Intro/app_flask_intro.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)    # Initialising the data base
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False) # 'nullable = False' because we don't want this to be left blank 
-    completed = db.Column(db.Integer, default = 0)  # Ignore this line, the completed column is never used (???)
+ #   completed = db.Column(db.Integer, default = 0)  # Ignore this line, the completed column is never used (???)
     date_created = db.Column(db.DateTime, default = datetime.now)
 
     def __repr__(self):     # a function to return a string everytime we create a new element
@@ -37,6 +37,36 @@ def index():        # Define the function for the previous route
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html' , tasks=tasks)
 
+
+@app.route('/delete/<int:id>')  # This is the "delete" part of the app 
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')   # This send us back to our homepage
+    except:
+        return 'There was a problem with the task'
+    
+
+@app.route('/update/<int:id>' , methods = ['GET' , 'POST'] )  # This is the "delete" page of the app 
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating the task'    
+    else:
+        return render_template('update.html' , task = task)
+
+
+
 if __name__ == '__main__':
-    app.run(host = 'localhost', port = 5001, debug=True)     # = True, so if we have any errors they will pop-up and we can see
+    app.run(host = 'localhost', port = 5001, debug=True)     # '= True', so if we have any errors they will pop-up and we can see
 
